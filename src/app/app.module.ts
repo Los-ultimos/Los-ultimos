1a14a15ffd86c98dc358f652a897a308dc40cd33
nickygon <nikita27111@hotmail.com>
2021-11-28 17:57:01 -0400

    subiendo el menu sidenav. Esperando implementacion con la pagina principal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatMenuModule} from '@angular/material/menu';
 import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatListModule} from '@angular/material/list'
-import {MatToolbarModule} from '@angular/material/toolbar'
-import {MatButtonModule} from '@angular/material/button'
+import {MatIconModule} from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import {MatListModule} from '@angular/material/list';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
 
 import {MatChipsModule} from '@angular/material/chips';
-
+import { SidenavMenuComponent } from './sidenav-menu/sidenav-menu.component';
+import { HeaderComponent } from './header/header.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidenavMenuComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
@@ -21,9 +26,12 @@ import {MatChipsModule} from '@angular/material/chips';
     BrowserAnimationsModule,
     MatSidenavModule,
     MatListModule,
+    MatMenuModule,
     MatButtonModule,
     MatChipsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatIconModule,
+    MatDividerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
