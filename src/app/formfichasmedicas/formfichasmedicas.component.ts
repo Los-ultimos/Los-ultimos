290e9dfafc22c5c8398cf15188952733594e65e5
nickygon <nikita27111@hotmail.com>
2021-12-05 01:07:19 -0400

    prueba de ingreso de datos del formulario

--- src/app/formfichasmedicas/formfichasmedicas.component.ts
+++ src/app/formfichasmedicas/formfichasmedicas.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-formfichasmedicas',
@@ -7,11 +10,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormfichasmedicasComponent implements OnInit {
 
-  constructor() { }
+  formReactive:FormGroup;
+  
+  constructor(private formBuilder:FormBuilder) {
+
+    this.formReactive=this.formBuilder.group({
+
+      
+      
+      numeroficha:['',[Validators.required]],
+      tipoconsulta:['',[Validators.required]],
+      veterinario:['',[Validators.required]],
+      atencionalbergue:['',[Validators.required]],
+      fecharegistroficha:['',[Validators.required]],
+      nombreanimal:['',[Validators.required]],
+      especie:['',[Validators.required]],
+      sexo:['',[Validators.required]],
+      raza:['',[Validators.required]],
+      color:['',[Validators.required]],
+      fechanacimientoanimal:['',[Validators.required]],
+      vacunas:['',[Validators.required]],
+      edad:['',[Validators.required]],
+      esterilizado:['',[Validators.required]],
+      
+
+      nombredueno:['',[Validators.required]],
+      ci:['',[Validators.required]],
+      telefono:['',[Validators.required]],
+      distrito:['',[Validators.required]],
+      barrio:['',[Validators.required]],
+      zona:['',[Validators.required]],
+      direccion:['',[Validators.required]],
+      email:['',[Validators.required,Validators.email]]
+    });
+     }
+
+  
+
+  ngOnInit(): void {
+
+
+  }
+
+  onShowAll(){
+    console.log('DDDD',this.formReactive.value);
+  }
+
 
   imageSrc = "https://cnnespanol.cnn.com/wp-content/uploads/2021/10/211028182411-rba-shiba-inu-full-169-e1635461449925.jpg?quality=100&strip=info"
 
-  ngOnInit(): void {
-  }
 
 }
--- src/app/formreporte/formreporte.component.ts
+++ src/app/formreporte/formreporte.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 @Component({
   selector: 'app-formreporte',
   templateUrl: './formreporte.component.html',
@@ -7,9 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormreporteComponent implements OnInit {
 
-  constructor() { }
+  formReactive:FormGroup;
+  
+  constructor(private formBuilder:FormBuilder) {
+
+    this.formReactive=this.formBuilder.group({
+
+     fuente:['',[Validators.required,Validators.email]],
+      criterio:['',[Validators.required]],
+      desde:['',[Validators.required]],
+      hasta:['',[Validators.required]]
+    });
+     }
+
+  
 
   ngOnInit(): void {
+
+
+  }
+
+  onShowAll(){
+    console.log('DDDD',this.formReactive.value);
   }
 
 }
