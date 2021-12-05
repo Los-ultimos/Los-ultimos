c31a8c9b700dddf60084c8912deb6664fee4689f
nickygon <nikita27111@hotmail.com>
2021-12-05 00:37:51 -0400

    empezando a guardar mas datos de otro formulario

--- src/app/formcarnet/formcarnet.component.ts
+++ src/app/formcarnet/formcarnet.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-formcarnet',
@@ -7,9 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormcarnetComponent implements OnInit {
 
-  constructor() { }
+  formReactive:FormGroup;
+  
+  constructor(private formBuilder:FormBuilder) {
+
+    this.formReactive=this.formBuilder.group({
+      fechadeemisioncarnet:['',[Validators.required]],
+      fechadeexpiracioncarnet:['',[Validators.required]],
+      codigocarnet:['',[Validators.required]],
+      fechaderegistrocarnet:['',[Validators.required]],
+    });
+     }
+
+  
 
   ngOnInit(): void {
+
+
+  }
+
+  onShowAll(){
+    console.log('DDDD',this.formReactive.value);
   }
 
 }
